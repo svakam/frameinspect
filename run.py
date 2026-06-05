da60fc9aa73321186af6aeb39c68b37a8b28253c
vikra <vikramakam@hotmail.com>
2026-06-05 12:33:22 -0700

    loading and inspecting frame

--- run.py
+++ run.py
@@ -6,19 +6,30 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from ultralytics import YOLO
 
-print("OpenCV:", cv2.__version__)
+print("OpenCV:", cv.__version__)
 print("NumPy:", np.__version__)
 print("Imported")
-
  
 #%% Load and inspect frame
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+img = cv.imread("./img/ai_baby_color.png") # load as default BGR uint8
 if img is None:
     sys.exit("Could not read the image.")
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB) # for mpl
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+print("Shape (color):", img.shape)
+print("Shape (gray):", gray.shape)
+print("Dtype:", gray.dtype)
+print("Min max pixels:", gray.min(), gray.max())
+
+# plot
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1); plt.imshow(img_rgb); plt.title("Color")
+plt.subplot(1, 2, 2); plt.imshow(gray, cmap="gray"); plt.title("Grayscale")
+plt.show()
+# cv.imshow("Display window", img)
+# k = cv.waitKey(10000)
+
+
 # %%
